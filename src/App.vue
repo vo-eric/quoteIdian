38ace89efde769a8303c0e7ef86d188b3d16ae93
Eric Vo <vo.eric@gmail.com>
2018-02-21 21:08:12 -0500

    add append and remove quote functionality

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
     <div class="container">
-    
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      <app-quote-view :quotes="quotes" @quoteRemoved="removeQuote"></app-quote-view>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">
+            Click on a quote to delete it
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+  import QuoteView from './components/Quoteview.vue';
+  import NewQuote from './components/NewQuote.vue'
+
+  export default {
+    data: function() {
+      return {
+        quotes: [
+            'Here\'s a placeholder quote'
+        ],
+        maxQuotes: 10
+      }
+    },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      },
+      removeQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    },
+    components: {
+      appQuoteView: QuoteView,
+      appNewQuote: NewQuote
     }
+  }
 </script>
 
 <style>
---
+++ src/components/Quoteview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, index) in quotes" @click.native="removeQuote(index)">{{ quote }}</app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue'
+  export default {
+    props: ['quotes'],
+    components: {
+      appQuote: Quote
+    },
+    methods: {
+      removeQuote(index) {
+        this.$emit('quoteRemoved', index);
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
